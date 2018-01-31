a9b9966191c14c141f6c5fcd565fd026ab3fc1ad
Murlock <l.malinowski86@gmail.com>
2018-01-31 20:13:21 +0100

    add variables

--- day2/variables.py
+++ day2/variables.py
@@ -8,7 +8,7 @@ print(temperature)
 print(name)
 print(programmer)
 print(nothing)
-# @TODO: kiwi kiwi
+# @TODO: kiwi kiwi  #przypomina o czyms do dodania w pliku
 age = 111
 print(age)
 type(age)   #wyswietla typ danych zmiennej
