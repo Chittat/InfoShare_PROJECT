89ed65903a881778228823721479fa4e4927b374
Murlock <l.malinowski86@gmail.com>
2018-01-31 20:11:35 +0100

    added "z rabararbarem"

---
+++ day2/variables.py
@@ -0,0 +1,19 @@
+age = 100                #int
+temperature = 6.6       #float
+name = "Janusz"         #string
+programmer = True       #bool
+nothing = None          #Nonetype
+print(age)
+print(temperature)
+print(name)
+print(programmer)
+print(nothing)
+# @TODO: kiwi kiwi
+age = 111
+print(age)
+type(age)   #wyswietla typ danych zmiennej
+print(type(age))
+print(type(temperature))
+print(type(name))
+print(type(programmer))
+print(type(nothing))
