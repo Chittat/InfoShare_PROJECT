dadd20cf591e7b2976b84879906deaf72ab9cbc8
Murlock <l.malinowski86@gmail.com>
2018-01-31 20:25:40 +0100

    create programm o display name

---
+++ day2/user.py
@@ -0,0 +1,7 @@
+name = input('Podaj swoje imie: ')
+print(name)
+message = 'Twoje imie to: '
+print('Twoje imie to: ', name)   #zle podejscie do wyswietlania
+print(message, name)            #dobre podejscie
+#\n new line
+
